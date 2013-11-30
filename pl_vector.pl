53d0fde6d62a05584d1321124618d9af0d2f8079
Stephen A. Goss <steveth45@gmail.com>
2013-11-30 07:03:57 -0800

    vector_new -> vector_length, made vector_at more relational

--- pl_vector.pl
+++ pl_vector.pl
@@ -1,12 +1,18 @@
 %% pl_vector.pl
 %% vector predicates for swi-prolog
 
-vector_new(Vector, Length) :-
+vector_length(Vector, Length) :-
         functor(Vector, vector, Length).
 
 vector_at(Vector, Index, Value) :-
+        number(Index),
+        !,
         Arg is Index + 1,
         arg(Arg, Vector, Value).
+
+vector_at(Vector, Index, Value) :-
+        arg(Arg, Vector, Value),
+        Index is Arg - 1.
 
 vector_each_aux(Vector, CurrIndex, Length, Pred) :-
         vector_at(Vector, CurrIndex, Value),
@@ -28,18 +34,23 @@ vector_list(Vector, List) :-
 :- begin_tests(vector).
 
 test(vector_at) :-
-        vector_new(V, 3),
+        vector_length(V, 3),
         vector_at(V, 1, hello),
         vector_at(V, 0, ok),
         vector_at(V, 2, world),
         V = vector(ok,hello,world).
 
-test(vector_new) :-
-        vector_new(V, 3),
+test(vector_at) :-
+        vector_list(V, [1,2,2,3,4]),
+        findall(Index, vector_at(V, Index, 2), Indexes),
+        Indexes = [1,2].
+
+test(vector_length) :-
+        vector_length(V, 3),
         V = vector(_,_,_).
 
 test(vector_each) :-
-        vector_new(V, 5),
+        vector_length(V, 5),
         vector_each(V, =(hello)),
         V = vector(hello, hello, hello, hello, hello).
 
